5acbde03de561ef7d968d74e9a468d3d78f58f23
Sai Hemanth <saihrd@amazon.com>
2025-10-07 22:34:07 +0530

    Created a SpingBoot Project

---
+++ src/main/java/org/example/bean/UserConfig.java
@@ -0,0 +1,14 @@
+package org.example.bean;
+
+public class UserConfig {
+    private String name;
+    private String className;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
